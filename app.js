ea60d2613e77313d6349359d6f177929501d9b60
daudi13 <davouma13@gmail.com>
2021-05-20 06:57:53 +0300

    commit practice

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
-const elJoke = document.getElementById('joke');
+const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-jokeBtn.addEventListener('click', generatejoke)
+jokeBtn.addEventListener('click', generateJoke)
 
-generatejoke();
+generateJoke();
 
-async function generatejoke() {
+async function generateJoke() {
     const config = {
         headers: {
             'Accept':'application/json'
         }
     }
-    // USING DOT THEN
-    // fetch('https://icanhazdadjoke.com/', config).then(response => response.json()).then((data) => {
-    //     elJoke.innerHTML = data.joke
-    // })
 
-    // USING ASYNC AWAIT
-
-    const response = await fetch('https://icanhazdadjoke.com', config);
-
+    const response = await fetch('https://icanhazdadjoke.com/', config);
     const data = await response.json();
-
-    elJoke.innerHTML = data.joke
-
+    jokeEl.innerHTML = data.joke
 
 }
